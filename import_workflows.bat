cf1b97ed9982ef0fcf50792d1812708d9688a895
vijaychothe <67112037+vijaychothe@users.noreply.github.com>
2020-07-17 16:10:39 +0530

    Add files via upload

---
+++ import_workflows.bat
@@ -0,0 +1,7 @@
+CALL E:\SPLM\DevOp_TC12\TR\tc_menu\XE_config1
+
+plmxml_import -u=infodba -p=infodba -g=dba -transfermode=workflow_template_overwrite -ignore_originid -xml_file=E:\SPLM\Deployment_Center\Repository\WorkFlow\__Find_Home_Folders.xml
+
+
+PAUSE
+echo Finish 
